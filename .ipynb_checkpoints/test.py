bd15339b47ceb3d9ef81fd95336ae0550a9c2381
Colin BERNARD <colin.bernard@ymail.com>
2023-04-27 20:26:19 +0200

    Pycodestyle check

--- .ipynb_checkpoints/test.py
+++ .ipynb_checkpoints/test.py
@@ -1,28 +1,31 @@
 import matplotlib.pyplot as plt
 from skimage import feature, transform, io
 from skimage.color import rgb2gray
-from skimage.feature import plot_matches, ORB
+from skimage.feature import ORB
 from skimage.measure import ransac
 from skimage.util import img_as_ubyte
 import numpy as np
 import math
 
+plt.rcParams['figure.figsize'] = (10, 10)
 
-""" On charge deux images et on les fusionne en mode panoramique. On utilise 
+
+""" On charge deux images et on les fusionne en mode panoramique. On utilise
     ORB pour trouver les points d'intérêts et RANSAC pour les aligner.
 """
 
-def trim_image(image, x_offset=0) :
+
+def trim_image(image, x_offset=0):
     mask = image == 0
     rows = np.nonzero((~mask).sum(axis=1))
     cols = np.nonzero((~mask).sum(axis=0))
-    trimmed = image[rows[0].min():rows[0].max()+1, cols[0].min():cols[0].max()-x_offset,:]
+    trimmed = image[rows[0].min():rows[0].max()+1,
+                    cols[0].min():cols[0].max()-x_offset,
+                    :]
     return trimmed
 
 
-plt.rcParams['figure.figsize'] = (10, 10)
-
-def merge_images(img_1, img_2, keypoints_2 = None, descriptors_2 = None):
+def merge_images(img_1, img_2, keypoints_2=None, descriptors_2=None):
     """ Fusionne deux images en mode panoramique """
 
     orb = ORB()
@@ -37,7 +40,9 @@ def merge_images(img_1, img_2, keypoints_2 = None, descriptors_2 = None):
         descriptors_2 = orb.descriptors
 
     """ On trouve les correspondances """
-    matches = feature.match_descriptors(descriptors_1, descriptors_2, cross_check=True)
+    matches = feature.match_descriptors(descriptors_1,
+                                        descriptors_2,
+                                        cross_check=True)
 
     if keypoints_1 is None or keypoints_2 is None:
         print("No keypoints found")
@@ -46,9 +51,12 @@ def merge_images(img_1, img_2, keypoints_2 = None, descriptors_2 = None):
     src = keypoints_1[matches[:, 0]][:, ::-1]
     dst = keypoints_2[matches[:, 1]][:, ::-1]
 
-
     """ On aligne les images """
-    model_robust, inliers = ransac((src, dst), transform.AffineTransform, min_samples=3, residual_threshold=2, max_trials=100)
+    model_robust, inliers = ransac((src, dst),
+                                   transform.AffineTransform,
+                                   min_samples=3,
+                                   residual_threshold=2,
+                                   max_trials=100)
 
     # La rotation retournée ne correspond pas toujours à la rotation appliquée
     # par transform.warp ci-dessous, laissant une partie du triangle noir à
@@ -57,10 +65,12 @@ def merge_images(img_1, img_2, keypoints_2 = None, descriptors_2 = None):
     print(f'Rotation : {math.degrees(model_robust.rotation)} deg')
     print(f'{x_offset} px to delete on X axis')
 
-
     """ On met la deuxième image à sa place """
-    img_2_warped = img_as_ubyte(transform.warp(img_2, model_robust, output_shape=(img_1.shape[0], img_1.shape[1] + img_2.shape[1])))
-
+    img_2_warped = img_as_ubyte(
+        transform.warp(img_2,
+                       model_robust,
+                       output_shape=(img_1.shape[0],
+                                     img_1.shape[1] + img_2.shape[1])))
 
     """ On merge les deux images """
     # TODO: Blending
@@ -82,7 +92,7 @@ if __name__ == "__main__":
         "images/small_image4.jpg",
         "images/small_image5.jpg"
     ]
-    
+
     result = io.imread(l_images[-1])
 
     for i in range(1, len(l_images)):
